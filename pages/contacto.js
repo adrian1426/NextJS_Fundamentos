f8886fa88344f64c5e8616c5f0570e3b01247723
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-01-22 15:40:36 -0600

    next bases y primera ruta LINK

---
+++ pages/contacto.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const contacto = () => {
+  return (
+    <div>
+      <h1>Mis contactos</h1>
+      <Link href="/">Inicio</Link>
+    </div>
+  );
+};
+
+export default contacto;
