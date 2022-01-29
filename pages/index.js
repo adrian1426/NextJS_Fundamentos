9668960ebb1697e609023d1de700c4be968ba675
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-01-29 17:06:08 -0600

    Componente Image Next

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <p>Hola Next.js</p>
       <Link href="/productos">Productos</Link>
+      <br />
+      <Image
+        src='/leon.jpg'
+        width={400}
+        height={400}
+        alt="Leon"
+      />
     </div>
   )
 }
